0f706f609cfd147a4f654e60102c6367bd1be2c2
智恒 <tomdeng.dzw@alibaba-inc.com>
2016-02-13 15:51:30 +0800

    init

---
+++ MobilePhoto/Categoryer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MobilePhoto
+{
+    public class Categoryer
+    {
+        public static void Category(String srcDir, String destDir)
+        {
+            var dir = new DirectoryInfo(srcDir);
+            dir.GetFiles().ToList().ForEach(fileInfo =>
+            {
+                MoveFileTo(fileInfo, destDir);
+            });
+            dir.GetDirectories().ToList().ForEach(dirInfo =>
+            {
+                Category(dirInfo.FullName, destDir);
+            });
+        }
+
+        private static void MoveFileTo(FileInfo srcFileInfo, String destDir)
+        {
+            string createDate = srcFileInfo.LastWriteTime.ToString("yyyyMM");
+            string ext = srcFileInfo.Extension;
+            string imagesExt = "jpg|jpeg|png";
+            string vedioExt = "mp4|3gp";
+
+            string path = destDir + @"\";
+            if (Regex.IsMatch(ext, imagesExt, RegexOptions.IgnoreCase))
+            {
+                path += @"照片\" + createDate + @"\";
+            }
+            else if (Regex.IsMatch(ext, vedioExt, RegexOptions.IgnoreCase))
+            {
+                path += @"视频\" + createDate + @"\";
+            }
+            else
+            {
+                path += @"其他\" + createDate + @"\";
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            srcFileInfo.MoveTo(path + srcFileInfo.Name);
+        }
+    }
+}
---
+++ MobilePhoto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args == null || args.Length == 0)
+            {
+                System.Console.WriteLine("Please input source dir");
+                return;
+            }
+
+            String srcDir = "";
+            String destDir = "";
+
+            if(args.Length == 1)
+            {
+                srcDir = args[0];
+                destDir = System.Environment.CurrentDirectory;
+            }
+            else
+            {
+                srcDir = args[0];
+                destDir = args[1];
+            }
+
+            Categoryer.Category(srcDir, destDir);
+
+            System.Console.WriteLine("Finished!");
+            System.Console.Read();
+        }
+    }
+}
